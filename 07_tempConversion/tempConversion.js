12b75a617b3597b2c22a6f7b9b8940b86459b024
Manikanta528 <chmanikanta528@gmail.com>
2022-03-09 05:51:17 +0530

    Solved 7th pb so part 4 exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(fharenheitInput) {
+  let celsiusRes = (fharenheitInput - 32 ) * 5/9;
+  //checking if the celsiusResult has the decimal point .
+  if(celsiusRes % 1 !=0){
+    let roundCelAns = Math.round(celsiusRes*10)/10;
+    return roundCelAns;
+  }
+  return celsiusRes;
 };
 
-const ctof = function() {
-
+const ctof = function(celsisusInput) {
+  let fharenheitRes = celsisusInput * 9/5 + 32;
+  if(fharenheitRes % 1 !=0){
+    let roundFhaAns = Math.round(fharenheitRes*10)/10;
+    return roundFhaAns;
+  }
+  return fharenheitRes;
 };
 
 // Do not edit below this line
